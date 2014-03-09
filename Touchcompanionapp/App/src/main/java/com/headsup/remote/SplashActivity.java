450a1952fefb4162284bfd2e73a512a37396d543
Tim <timrlw@gmail.com>
2014-03-09 13:05:12 +0000

    add splash screen

---
+++ Touchcompanionapp/App/src/main/java/com/headsup/remote/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.headsup.remote;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class SplashActivity extends ActionBarActivity {
+
+	private static final long SPLASH_SCREEN_DELAY = 1000;
+	private static boolean sApplicationLoaded = false;
+
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+        View view = findViewById(R.id.layout_splash);
+
+		if (sApplicationLoaded) {
+			launchApplication();
+		} else {
+            view.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    launchApplication();
+                }
+            }, SPLASH_SCREEN_DELAY);
+        }
+	}
+
+
+	private void launchApplication() {
+		if (!isFinishing()) {
+			sApplicationLoaded = true;
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+			finish();
+		}
+	}
+}
